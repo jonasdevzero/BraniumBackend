af307e7d634f6af6fda3f50be560896ea7a63cca
jonasdevzero <jonasdevzero@gmail.com>
2024-01-01 16:38:42 -0300

    fix: add multipart in create profile endpoint

--- src/presentation/controllers/profile/CreateProfileController.ts
+++ src/presentation/controllers/profile/CreateProfileController.ts
@@ -1,11 +1,19 @@
 import { inject } from '@container';
 import { CreateProfile } from '@domain/use-cases/profile';
-import { controller, middlewares, route } from '@presentation/decorators';
+import {
+	controller,
+	middlewares,
+	route,
+	upload,
+} from '@presentation/decorators';
 import { response } from '@presentation/helpers';
 import { Controller, HttpRequest, HttpResponse } from '@presentation/protocols';
 import { CreateProfileValidator } from '@presentation/validators/profile';
 
+const sizeLimit = 2 * 1024 * 1024; // 2 MB
+
 @controller()
+@upload({ mimeTypes: ['image/png', 'image/jpg', 'image/jpeg'], sizeLimit })
 @middlewares(CreateProfileValidator)
 @route.post('profile', '/')
 export class CreateProfileController implements Controller {
