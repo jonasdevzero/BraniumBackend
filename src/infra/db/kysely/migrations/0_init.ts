ddcc70effb726549918d440bb79afb2ad6b425c3
jonasdevzero <jonasdevzero@gmail.com>
2024-02-01 09:38:13 -0300

    feat: migration

--- src/infra/db/kysely/migrations/0_init.ts
+++ src/infra/db/kysely/migrations/0_init.ts
@@ -69,10 +69,137 @@ export async function up(db: Kysely<Database>): Promise<void> {
 			(cb) => cb.onUpdate('cascade').onDelete('cascade')
 		)
 		.execute();
+
+	await db.schema
+		.createTable('group')
+		.ifNotExists()
+		.addColumn('id', 'uuid', (col) => col.primaryKey())
+		.addColumn('name', 'text', (col) => col.notNull())
+		.addColumn('image', 'text')
+		.addColumn('description', 'text')
+		.addColumn('createdAt', 'timestamp', (col) =>
+			col.notNull().defaultTo(sql`now()`)
+		)
+		.addColumn('updatedAt', 'timestamp', (col) =>
+			col.notNull().defaultTo(sql`now()`)
+		)
+		.execute();
+
+	await db.schema
+		.createType('GroupUserRole')
+		.asEnum(['OWNER', 'ADMIN', 'ROLE'])
+		.execute();
+
+	await db.schema
+		.createTable('groupUser')
+		.ifNotExists()
+		.addColumn('groupId', 'uuid', (col) => col.notNull())
+		.addColumn('userId', 'uuid', (col) => col.notNull())
+		.addColumn('role', sql`"GroupUserRole"`, (col) => col.notNull())
+		.addColumn('createdAt', 'timestamp', (col) =>
+			col.notNull().defaultTo(sql`now()`)
+		)
+		.addPrimaryKeyConstraint('pk_groupUser', ['groupId', 'userId'])
+		.addForeignKeyConstraint('fk_groupUser_group', ['groupId'], 'group', ['id'])
+		.addForeignKeyConstraint('fk_groupUser_user', ['userId'], 'profile', ['id'])
+		.execute();
+
+	await db.schema
+		.createType('MessageType')
+		.asEnum(['TEXT', 'IMAGE', 'VIDEO', 'AUDIO', 'FILE', 'MIX'])
+		.execute();
+
+	await db.schema
+		.createTable('message')
+		.ifNotExists()
+		.addColumn('id', 'uuid', (col) => col.primaryKey())
+		.addColumn('senderId', 'uuid', (col) => col.notNull())
+		.addColumn('replyId', 'uuid')
+		.addColumn('groupId', 'uuid')
+		.addColumn('message', 'text')
+		.addColumn('type', sql`"MessageType"`, (col) => col.notNull())
+		.addColumn('createdAt', 'timestamp', (col) =>
+			col.defaultTo(sql`now()`).notNull()
+		)
+		.addColumn('updatedAt', 'timestamp')
+		.addForeignKeyConstraint('fk_message_sender', ['senderId'], 'profile', [
+			'id',
+		])
+		.addForeignKeyConstraint('fk_message_reply', ['replyId'], 'message', ['id'])
+		.addForeignKeyConstraint('fk_message_group', ['groupId'], 'group', ['id'])
+		.execute();
+
+	await db.schema
+		.createTable('messageUser')
+		.ifNotExists()
+		.addColumn('messageId', 'uuid', (col) => col.notNull())
+		.addColumn('userId', 'uuid', (col) => col.notNull())
+		.addColumn('key', 'text', (col) => col.notNull())
+		.addPrimaryKeyConstraint('pk_messageUser', ['messageId', 'userId'])
+		.addForeignKeyConstraint(
+			'fk_messageUser_message',
+			['messageId'],
+			'message',
+			['id']
+		)
+		.addForeignKeyConstraint('fk_messageUser_user', ['userId'], 'profile', [
+			'id',
+		])
+		.execute();
+
+	await db.schema
+		.createType('MessageFileType')
+		.asEnum(['IMAGE', 'VIDEO', 'AUDIO', 'FILE'])
+		.execute();
+
+	await db.schema
+		.createTable('messageFile')
+		.ifNotExists()
+		.addColumn('id', 'uuid', (col) => col.primaryKey())
+		.addColumn('messageId', 'uuid', (col) => col.notNull())
+		.addColumn('key', 'text', (col) => col.notNull())
+		.addColumn('type', sql`"MessageFileType"`, (col) => col.notNull())
+		.addForeignKeyConstraint(
+			'fk_messageFile_message',
+			['messageId'],
+			'message',
+			['id']
+		)
+		.execute();
+
+	await db.schema
+		.createTable('messageFileUser')
+		.ifNotExists()
+		.addColumn('fileId', 'uuid', (col) => col.notNull())
+		.addColumn('userId', 'uuid', (col) => col.notNull())
+		.addColumn('key', 'text', (col) => col.notNull())
+		.addPrimaryKeyConstraint('pk_messageFileUser', ['fileId', 'userId'])
+		.addForeignKeyConstraint(
+			'fk_messageFileUser_file',
+			['fileId'],
+			'messageFile',
+			['id']
+		)
+		.addForeignKeyConstraint('fk_messageFileUser_user', ['userId'], 'profile', [
+			'id',
+		])
+		.execute();
 }
 
 export async function down(db: Kysely<Database>): Promise<void> {
-	await db.schema.dropTable('invite').ifExists().execute();
+	await db.schema.dropTable('messageFileUser').ifExists().execute();
+	await db.schema.dropTable('messageFile').ifExists().execute();
+	await db.schema.dropType('MessageFileType').ifExists().execute();
+
+	await db.schema.dropTable('messageUser').ifExists().execute();
+	await db.schema.dropTable('message').ifExists().execute();
+	await db.schema.dropType('MessageType').ifExists().execute();
+
+	await db.schema.dropTable('groupUser').ifExists().execute();
+	await db.schema.dropType('GroupUserRole').ifExists().execute();
+	await db.schema.dropTable('group').ifExists().execute();
+
 	await db.schema.dropTable('contact').ifExists().execute();
+	await db.schema.dropTable('invite').ifExists().execute();
 	await db.schema.dropTable('profile').ifExists().execute();
 }
